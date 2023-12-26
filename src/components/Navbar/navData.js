8dca8045750822c00fe72bb68d618abbb169d17b
Abhinav <singhabhinav0923@gmail.com>
2023-12-26 22:56:15 +0530

    first

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,36 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Whatsapp',
+    url: 'https://api.whatsapp.com/send?phone=+917906215089',
+    icon: 'whatsapp',
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/abhinav-singh-2582b4226/',
+    icon: 'linkedin',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/abhinavsingh0923',
+    icon: 'github',
+  },
+];
---
+++ src/layouts/Home/Home.js
@@ -0,0 +1,185 @@
+import gamestackTexture2Large from 'assets/gamestack-list-large.jpg';
+import gamestackTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from 'assets/gamestack-list.jpg';
+import gamestackTextureLarge from 'assets/gamestack-login-large.jpg';
+import gamestackTexturePlaceholder from 'assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from 'assets/gamestack-login.jpg';
+import sliceTextureLarge from 'assets/slice-app-large.jpg';
+import sliceTexturePlaceholder from 'assets/slice-app-placeholder.jpg';
+import sliceTexture from 'assets/slice-app.jpg';
+import sprTextureLarge from 'assets/spr-lesson-builder-dark-large.jpg';
+import sprTexturePlaceholder from 'assets/spr-lesson-builder-dark-placeholder.jpg';
+import sprTexture from 'assets/spr-lesson-builder-dark.jpg';
+import { Footer } from 'components/Footer';
+import { Meta } from 'components/Meta';
+import { Intro } from 'layouts/Home/Intro';
+import { Profile } from 'layouts/Home/Profile';
+import { ProjectSummary } from 'layouts/Home/ProjectSummary';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Home.module.css';
+
+const disciplines = ['Application', 'Website', 'Blockchain'];
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const projectFour = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, projectThree, projectFour, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Meta
+        title="web + App Developer"
+        description="Design portfolio of  Abhinav singh — a web, application and blockchain developer."
+      />
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        disciplines={disciplines}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="Voting Application using bockchain"
+        description="Created a blockchain voting application using ethereum blockchain and the technology are used - Ethereum bockchain, Ganache, Truffle and the web3.js"
+        buttonText="View code"
+        buttonLink="https://github.com/abhinavsingh0923/spotify-clone/"
+        model={{
+          type: 'laptop',
+          alt: 'voting Application',
+          textures: [
+            {
+              srcSet: [sprTexture, sprTextureLarge],
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="spotify clone"
+        description="Created a spotify clone using blockchain and storing music files on the IPFS and the client side is working on Flutter."
+        buttonText="view code"
+        buttonLink="https://github.com/abhinavsingh0923/spotify-clone/"
+        model={{
+          type: 'phone',
+          alt: 'App login screen',
+          textures: [
+            {
+              srcSet: [gamestackTexture, gamestackTextureLarge],
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: [gamestackTexture2, gamestackTexture2Large],
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-3"
+        sectionRef={projectThree}
+        visible={visibleSections.includes(projectThree.current)}
+        index={3}
+        title="Ethereum Wallet"
+        description="Created an Ethereum wallet Cross-Platefrom Application using Flutter for the client side and using solidity Smart contract for the blockchain. "
+        buttonText="view code "
+        buttonLink="https://github.com/abhinavsingh0923/spotify-clone/"
+        model={{
+          type: 'phone',
+          alt: 'Annotating a biomedical image in the Slice app',
+          textures: [
+            {
+              srcSet: [gamestackTexture, gamestackTextureLarge],
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: [gamestackTexture2, gamestackTexture2Large],
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />
+
+      <ProjectSummary
+        id="project-4"
+        alternate
+        sectionRef={projectFour}
+        visible={visibleSections.includes(projectFour.current)}
+        index={4}
+        title="voice assistant Application "
+        description="Created an voice assistant application using client side as Flutter and the for the modeling/Answering we use the OpenAI ChatGpt api."
+        buttonText="view code "
+        buttonLink="https://github.com/abhinavsingh0923/spotify-clone/"
+        model={{
+          type: 'phone',
+          alt: 'App login screen',
+          textures: [
+            {
+              srcSet: [gamestackTexture, gamestackTextureLarge],
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: [gamestackTexture2, gamestackTexture2Large],
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />      
+      
+      <Profile
+      sectionRef={details}
+      visible={visibleSections.includes(details.current)}
+      id="details"
+    />
+      <Footer />
+    </div>
+  );
+};
